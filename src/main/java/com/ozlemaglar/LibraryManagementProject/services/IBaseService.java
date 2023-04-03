876f888160e2a06466433aec569b9f63ca1b1ef1
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2023-04-04 01:56:54 +0300

    IBaseService created

--- src/main/java/com/ozlemaglar/LibraryManagementProject/services/IBaseService.java
+++ src/main/java/com/ozlemaglar/LibraryManagementProject/services/IBaseService.java
@@ -2,16 +2,21 @@ package com.ozlemaglar.LibraryManagementProject.services;
 
 import com.ozlemaglar.LibraryManagementProject.entity.Book;
 import org.springframework.data.domain.Page;
+
 import java.util.Map;
 import java.util.Optional;
 
 
-public interface IBookService {
+public interface IBaseService<T> {
 
-    public Book save(Book book);
-    public Page<Book> getAllBooks(int page, int size);
-    public Optional<Book> find(Long id);
-    public Book update(Book book);
+    public T save(T entity);
+
+    public Page<T> getAll(int page, int size);
+
+    public Optional<T> find(Long id);
+
+    public T update(T entity);
+
     public Boolean delete(Long id);
 
 }

094fc4c858688e337155de17ca4fe31d65be9a54
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2023-04-10 21:38:27 +0300

    Author service and controller added

---
+++ src/main/java/com/ozlemaglar/LibraryManagementProject/controller/AuthorController.java
@@ -0,0 +1,44 @@
+package com.ozlemaglar.LibraryManagementProject.controller;
+
+import com.ozlemaglar.LibraryManagementProject.entity.Author;
+import com.ozlemaglar.LibraryManagementProject.services.AuthorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/author")
+@RequiredArgsConstructor
+public class AuthorController {
+
+    public final AuthorService authorService;
+
+    @PostMapping("/add")
+    public Author add(Author author) {
+        authorService.save(author);
+        return author;
+    }
+
+    @GetMapping("/")
+    public List<Author> getAll(){
+        return authorService.getAll();
+    }
+
+    @GetMapping("/find/{id}")
+    public Author find(@PathVariable(value = "id") Long id) {
+        return authorService.find(id).get();
+    }
+
+    @PutMapping("/update/{id}")
+    public Author update(Author author) {
+        return authorService.update(author);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Boolean delete(@PathVariable(value = "id") Long id) {
+        return authorService.delete(id);
+    }
+
+}
---
+++ src/main/java/com/ozlemaglar/LibraryManagementProject/services/AuthorService.java
@@ -0,0 +1,66 @@
+package com.ozlemaglar.LibraryManagementProject.services;
+
+
+import com.ozlemaglar.LibraryManagementProject.entity.Author;
+import com.ozlemaglar.LibraryManagementProject.repository.IAuthorRepo;
+import io.micrometer.common.util.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class AuthorService implements IBaseService<Author> {
+
+
+    private final IAuthorRepo authorRepo;
+
+    public AuthorService(IAuthorRepo authorRepo) {
+        this.authorRepo = authorRepo;
+    }
+
+    @Override
+    public Author save(Author author) {
+        authorRepo.save(author);
+        return author;
+    }
+
+
+    public List<Author> getAll(){
+        return (List<Author>) authorRepo.findAll();
+    }
+
+    @Override
+    public Optional<Author> find(Long id) {
+        Optional<Author> optionalAuthor = authorRepo.findById(id);
+        return optionalAuthor;
+    }
+
+    @Override
+    public Author update(Author author) {
+        if (StringUtils.isNotEmpty(author.getId().toString())) {
+            Optional<Author> optionalAuthor = authorRepo.findById(author.getId());
+
+            if (optionalAuthor.isPresent()) {
+                authorRepo.save(author);
+            }
+        } else {
+            throw new IllegalArgumentException("id bulunamadı");
+        }
+        return author;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        Optional<Author> optionalAuthor = authorRepo.findById(id);
+        if (optionalAuthor.isPresent()) {
+            authorRepo.deleteById(id);
+            return true;
+
+        } else {
+            throw new IllegalArgumentException("kayıt bulunamadı");
+        }
+    }
+
+}
